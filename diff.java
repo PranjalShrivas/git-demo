5f5a9fc2b52f1c08750ce887601052748ed36b94
Pranjal Shrivas <shrivas.5298@gmail.com>
2023-04-17 02:28:55 +0530

    diff changed

--- diff.java
+++ diff.java
@@ -4,6 +4,6 @@ public class diff{
     public static void main(String[] args) {
         int a=3;
         int b=4;
-        System.out.println(b/a);
+        System.out.println("The division is "+b/a);
     }
 }
